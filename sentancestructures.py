5170ab948f73913260766dc268751f8d752ada51
Alexandra Miranda <mirandae197@potsdam.edu>
2018-02-23 01:03:48 -0500

    pattern.en
    using pattern.en to change tenses and add articles so that it won't just generate sentences from arrays

--- sentancestructures.py
+++ sentancestructures.py
@@ -3,21 +3,30 @@
 # senior project
 
 from random import *
+from pattern.en import conjugate, lemma, lexeme, referenced, article
 
-verbs = ["runs", "walks", "climbs", "crawls", "sits"]
-nouns = ["girl", "boy", "chicken", "cat", "dog"]
+verbs = ["run", "walk", "climb", "crawl", "sit"]
+nouns = ["girl", "boy", "chicken", "cat", "dog", "apple"]
 article = "the"
-sentencestruct1 = ["<art>", "<noun>", "<verb>"]
+sentencestruct1 = ["past", "<art>", "<noun>", "<verb>"]
+sentencestruct2 = ["pres", "<art>", "<noun>", "<verb>"]
 
-def makeSent():
-	noun = randint(0, length(verbs))
-	verb = randint(0, length(nouns))
-	for i in range(0, length(sentencestruct1)):
-		if(sentencestruct1[i] == "<art>"):
-			print article
-		elif(sentencestruct1[i] == "<noun>"):
-			print nouns[noun]
-		elif(sentencestruct1[i] == "<verb>"):
-			print verbs[verb]
+def makeSent(struct):
+	noun = randint(0, len(nouns)-1)
+	verb = randint(0, len(verbs)-1)
+	tense = ''
+	if(struct[0] == "past"):
+		tense = 'sgp'
+	elif(struct[0] == "pres"):
+		tense = 'sg'
+	for i in range(1, len(struct)):
+		#if(struct[i] == "<art>"):
+			#print article + ' ',
+		if(struct[i] == "<noun>"):
+			print referenced(nouns[noun]) + ' ',
+		elif(struct[i] == "<verb>"):
+			print conjugate(verbs[verb], '3' + tense) + ' ',
+	print "\n"
 
-makeSent()
+makeSent(sentencestruct1)
+makeSent(sentencestruct2)
